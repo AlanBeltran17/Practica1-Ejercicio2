94177b65ab784567c7a6f8dbdbc60be79e9fcfba
Alan <alanbeltrancanaza4@gmail.com>
2023-12-15 08:14:47 -0400

    Agregar archivos de proyecto.

---
+++ 2/Clases/listas.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Clases
+{
+    internal class listas
+    {
+        
+        public List<int> MultiplosDeDos(List<int> lista)
+        {
+            List<int> multiplos2 = new List<int>();
+
+            foreach (int i in lista)
+            {
+                if (i % 2 == 0)
+                    if (!multiplos2.Contains(i))  multiplos2.Add(i); 
+            }
+            multiplos2.Sort();
+            return multiplos2;
+        }
+
+        internal List<int> MultiplosDeCinco(List<int> lista)
+        {
+            List<int> multiplos5 = new List<int>();
+
+            foreach (int i in lista)
+            {
+                if (i % 5 == 0)
+                    if (!multiplos5.Contains(i)) multiplos5.Add(i);
+            }
+            multiplos5.Sort();
+            return multiplos5;
+        }
+
+        internal List<int> Perfectos(List<int> lista)
+        {
+            List<int> perfectos = new List<int>();
+
+            foreach (int i in lista)
+            {
+                if (EsPerfecto(i))
+                    if (!perfectos.Contains(i))  perfectos.Add(i);
+            }
+            perfectos.Sort();
+            return perfectos;
+        }
+
+        public bool EsPerfecto(int num)
+        {
+            int sum = 0;
+            for (int i = 1; i < num; i++)
+            {
+                if (num % i == 0)
+                    sum += i;
+            }
+
+            if (sum == num) return true;
+            return false;
+        }
+
+        internal List<int> Primos(List<int> lista)
+        {
+            List<int> primos = new List<int>();
+
+            foreach (int i in lista)
+            {
+                if (EsPrimo(i, i-1))
+                    if (!primos.Contains(i))  primos.Add(i);
+            }
+            primos.Sort();
+            return primos;
+        }
+
+        public bool EsPrimo(int num, int i)
+        {
+            if (i == 1) return true;
+            else if (num == 1 || num % i == 0) return false;
+            else return EsPrimo(num, i - 1);
+        }
+
+        //Mostrar Lista
+        public void MostrarLista(List<int> lista)
+        {
+            foreach (int i in lista)
+            {
+                Console.Write("{0} ", i);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 2/Program.cs
@@ -0,0 +1,62 @@
+﻿using _2.Clases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+
+        List<int> numeros = new List<int>();
+        int n;
+
+        Console.Write("¿Cuántos numeros quieres Añadir?:  ");
+        if (int.TryParse(Console.ReadLine(), out n))
+        {
+            Console.WriteLine("Añada {0} numeros", n);
+            for (int i = 0; i < n; i++)
+            {
+                if (int.TryParse(Console.ReadLine(), out int numero))
+                {
+                    numeros.Add(numero);
+                }
+                else
+                {
+                    Console.WriteLine("¡NO ES UN ENTERO!, por favor ingrese un número ENTERO");
+                    i--;
+                }
+            }
+
+            listas lista = new listas();
+
+            //Listas
+            List<int> MultiplosDe2 = lista.MultiplosDeDos(numeros);
+            List<int> Primos = lista.Primos(numeros);
+            List<int> MultiplosDe5 = lista.MultiplosDeCinco(numeros);
+            List<int> Perfectos = lista.Perfectos(numeros);
+            Console.WriteLine();
+            Console.Write("Lista 1:   ");
+            lista.MostrarLista(MultiplosDe2);
+            Console.Write("Lista 2:   ");
+            lista.MostrarLista(Primos);
+            Console.Write("Lista 3:   ");
+            lista.MostrarLista(MultiplosDe5);
+            Console.Write("Lista 4:   ");
+            lista.MostrarLista(Perfectos);
+
+        }
+        else
+        {
+            Console.WriteLine("No es un número válido para la cantidad de números a ingresar.");
+        }
+
+
+
+        Console.ReadKey();
+        }
+    }
